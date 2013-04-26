5d6224fd3882f9e16852f8ca331aee391419cc48
finihoge <nicolai.hoge@finn.no>
2013-04-26 12:43:17 +0200

    Implemented less than 2

--- src/main/java/no/finntech/kata/World.java
+++ src/main/java/no/finntech/kata/World.java
@@ -3,6 +3,19 @@ package no.finntech.kata;
 public final class World {
 
     public boolean[][] nextGeneration(boolean[][] current) {
-        return new boolean[][] {};
+        boolean[][] nextGeneration = new boolean[current.length][current[0].length];
+        for (int x = 0; x < current.length; x++) {
+            for (int y = 0; y < current[0].length; y++) {
+                int liveNeighbours = livingNeigbours();
+                if (liveNeighbours < 2) {
+                    nextGeneration[x][y] = false;
+                }
+            }
+        }
+        return nextGeneration;
+    }
+
+    private int livingNeigbours() {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
     }
 }
