e272e0d6d09da65a57d6cfc52bd9f272addde8ca
finihoge <nicolai.hoge@finn.no>
2013-04-26 13:11:32 +0200

    nit

--- src/main/java/no/finntech/kata/World.java
+++ src/main/java/no/finntech/kata/World.java
@@ -4,18 +4,38 @@ public final class World {
 
     public boolean[][] nextGeneration(boolean[][] current) {
         boolean[][] nextGeneration = new boolean[current.length][current[0].length];
-        for (int x = 0; x < current.length; x++) {
-            for (int y = 0; y < current[0].length; y++) {
-                int liveNeighbours = livingNeigbours();
+        for (int y = 0; y < current.length; y++) {
+            for (int x = 0; x < current[0].length; x++) {
+                int liveNeighbours = livingNeigbours(x, y, current);
                 if (liveNeighbours < 2) {
                     nextGeneration[x][y] = false;
+                } if (liveNeighbours == 2 || liveNeighbours == 3) {
+                    nextGeneration[x][y] = true;
                 }
             }
         }
         return nextGeneration;
     }
 
-    private int livingNeigbours() {
-        return 0;  //To change body of created methods use File | Settings | File Templates.
+    private int livingNeigbours(int x, int y, boolean[][] current) {
+        int result = 0;
+        if (living(x-1, y-1, current)) result++;
+        if (living(x-1, y, current)) result++;
+        if (living(x-1, y+1, current)) result++;
+        if (living(x, y-1, current)) result++;
+        if (living(x, y+1, current)) result++;
+        if (living(x+1, y-1, current)) result++;
+        if (living(x+1, y, current)) result++;
+        if (living(x+1, y+1, current)) result++;
+        System.out.println(x + ":" + y + " : neighbours : " + result);
+        return result;
+    }
+
+    private boolean living(int x, int y, boolean[][] current) {
+        return exists(x, y, current) && current[x][y];
+    }
+
+    private boolean exists(int x, int y, boolean[][] current) {
+        return !(x < 0 || y < 0 || x > current.length -1 || y > current[0].length - 1);
     }
 }
